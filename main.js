f742b2b2dd025e1c80cf8bb1eb19c90294dcf570
Cangerana <mrjoaovictorc@gmail.com>
2022-01-11 02:14:50 -0300

    Added dat.gui

--- main.js
+++ main.js
@@ -1,15 +1,41 @@
+// https://www.youtube.com/watch?v=YK1Sw_hnm58
+// import * as THREE from 'https://cdn.skypack.dev/three@0.126.1';
 import * as THREE from 'https://cdn.skypack.dev/three@0.126.1';
 import * as dat from 'dat.gui'
 
+function createPlaneGeometry() {
+    planeMesh.geometry.dispose()
+    planeMesh.geometry = new THREE.PlaneGeometry(
+    	world.plane.width,
+    	world.plane.height,
+    	world.plane.widthSegments,
+    	world.plane.heightSegments)
+
+    const { array } = planeMesh.geometry.attributes.position
+
+    for (let i = 0; i < array.length; i += 3) {
+        const x = array[i]
+        const y = array[i + 1]
+        const z = array[i + 2]
+        array[i+2] = z + Math.random()
+    }
+}
 
 const gui = new dat.GUI()
 const world = {
   plane: {
     width: 10,
+    height: 10,
+    heightSegments: 10,
+    widthSegments: 10
   }
 }
 
-gui.add(world.plane, 'width', 1, 500)
+gui.add(world.plane, 'width', 1, 20).onChange(createPlaneGeometry)
+
+gui.add(world.plane, 'height', 1, 20).onChange(createPlaneGeometry)
+gui.add(world.plane, 'widthSegments', 1, 200).onChange(createPlaneGeometry)
+gui.add(world.plane, 'heightSegments', 1, 200).onChange(createPlaneGeometry)
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, innerWidth / innerHeight, 0.1, 1000)
